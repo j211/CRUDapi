48507b8f03d1fbd3ced9441136f7c3b635a97671
Iuliia Dosaeva <julia-dos@yandex.ru>
2025-01-14 19:42:02 +0400

    updated CRUD api

--- src/main/java/com/example/CRUDapi/entities/Task.java
+++ src/main/java/com/example/CRUDapi/entities/Task.java
@@ -1,56 +1,24 @@
 package com.example.CRUDapi.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-
+@Data
 @Entity
+@Table(name = "tasks")
 public class Task {
-    private @Id
-    @GeneratedValue Long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
     private String title;
+
+    @Column
     private String description;
-    private Integer userId;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" + "id=" + this.id + "," +
-                " title='" + this.title + '\'' + "," +
-                " description='" + this.description + "," +
-                " userId='" + this.userId + '\'' + '}';
-    }
+    @Column
+    private int userId;
 }
 
